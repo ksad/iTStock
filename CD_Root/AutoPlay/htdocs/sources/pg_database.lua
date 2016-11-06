56f15eb7e8f4414ee9596f0e519240b81bdcc018
Karim SAD <ksad.karim@gmail>
2016-11-06 21:26:18 +0100

    [FIX]: Improving SQL server status check

--- CD_Root/AutoPlay/htdocs/sources/pg_database.lua
+++ CD_Root/AutoPlay/htdocs/sources/pg_database.lua
@@ -1,19 +1,49 @@
-function checkSQLServerStatus()
-	local checkSQLServer = Service.Query("mysql57", "mysql57");
+function checkSQLServerStatus(dbName, dbUser, dbPassword, dbAddress, dbPort)
+	if not dbName or not dbUser or not dbPassword or not dbPort or not dbAddress then
+		Crypto.BlowfishDecrypt("AutoPlay\\htdocs\\database\\db_access.enc", "AutoPlay\\htdocs\\database\\~tmp_db_access.txt", "securestockitwiththispassword");
+		local db_access = TextFile.ReadToTable("AutoPlay\\htdocs\\database\\~tmp_db_access.txt");
+		File.Delete("AutoPlay\\htdocs\\database\\~tmp_db_access.txt", false, false, false, nil);
 
-	Label.SetVisible("LB_SERVER_STATUS", true);
-	if checkSQLServer == 4 then
-		Label.SetText("LB_SERVER_STATUS", "Server is running");
-		Image.Load("IMG_SERVER_STATUS", "AutoPlay\\htdocs\\images\\icons\\server_ok.png");
-	elseif checkSQLServer == 0 then
-		Label.SetText("LB_SERVER_STATUS", "Server not found");
-		Image.Load("IMG_SERVER_STATUS", "AutoPlay\\htdocs\\images\\icons\\server_ko.png");
-	elseif checkSQLServer == 1 then
-		Label.SetText("LB_SERVER_STATUS", "Server stopped");
-		Image.Load("IMG_SERVER_STATUS", "AutoPlay\\htdocs\\images\\icons\\server_ko.png");
+		dbName = String.TrimLeft(db_access[1], "[DB_NAME]=");
+		dbUser = String.TrimLeft(db_access[2], "[USER]=");
+		dbPassword = String.TrimLeft(db_access[3], "[PASSWORD]=");
+		dbAddress = String.TrimLeft(db_access[4], "[ADDRESS]=");
+		dbPort = String.TrimLeft(db_access[5], "[PORT]=");
 	end
-	
-	return checkSQLServer;
+
+	if dbAddress == "localhost" or dbAddress == "127.0.0.1" then
+		sqlServerStatus = Service.Query("mysql57", "mysql57");
+
+		if sqlServerStatus == 0 then
+			serverStatusMsg = "not found";
+		elseif sqlServerStatus == 1 then
+			serverStatusMsg = "is not running";
+		elseif sqlServerStatus == 2 then
+			serverStatusMsg = "is starting";
+		elseif sqlServerStatus == 3 then
+			serverStatusMsg = "is stopping"
+		elseif sqlServerStatus == 4 then
+			serverStatusMsg = "is running"
+		elseif sqlServerStatus == 5 then
+			serverStatusMsg = "continue is pending";
+		elseif sqlServerStatus == 6 then
+			serverStatusMsg = "pause is pending"
+		elseif sqlServerStatus == 7 then
+			serverStatusMsg = "is paused";
+		elseif sqlServerStatus == -1 then
+			serverStatusMsg = "error";
+		end
+	else
+		MySQLConnection, err = MySQL:connect(dbName, dbUser, dbPassword, dbAddress, dbPort);
+		if err then
+			sqlServerStatus = 8;
+			serverStatusMsg = err;
+		else
+			sqlServerStatus = 4;
+			serverStatusMsg = "is running"
+		end
+	end
+	return "["..sqlServerStatus.."]"..serverStatusMsg;
 end
 
 function loadDbAccess ()
@@ -21,11 +51,39 @@ function loadDbAccess ()
 	local db_access = TextFile.ReadToTable("AutoPlay\\htdocs\\database\\~tmp_db_access.txt");
 	File.Delete("AutoPlay\\htdocs\\database\\~tmp_db_access.txt", false, false, false, nil);
 
-	Input.SetText("IN_DB_NAME_DBCF", String.TrimLeft(db_access[1], "[DB_NAME]="));
-	Input.SetText("IN_DB_USERNAME_DBCF", String.TrimLeft(db_access[2], "[USER]="));
-	Input.SetText("IN_DB_PASSWORD_DBCF", String.TrimLeft(db_access[3], "[PASSWORD]="));
-	Input.SetText("IN_DB_ADDRESS_DBCF", String.TrimLeft(db_access[4], "[ADDRESS]="));
-	Input.SetText("IN_DB_PORT_DBCF", String.TrimLeft(db_access[5], "[PORT]="));
+	dbName = String.TrimLeft(db_access[1], "[DB_NAME]=");
+	dbUser = String.TrimLeft(db_access[2], "[USER]=");
+	dbPassword = String.TrimLeft(db_access[3], "[PASSWORD]=");
+	dbAddress = String.TrimLeft(db_access[4], "[ADDRESS]=");
+	dbPort = String.TrimLeft(db_access[5], "[PORT]=");
+
+	Input.SetText("IN_DB_NAME_DBCF", dbName);
+	Input.SetText("IN_DB_USERNAME_DBCF", dbUser);
+	Input.SetText("IN_DB_PASSWORD_DBCF", dbPassword);
+	Input.SetText("IN_DB_ADDRESS_DBCF", dbAddress);
+	Input.SetText("IN_DB_PORT_DBCF", dbPort);
+
+	local serverStatus = checkSQLServerStatus(databaseName, userName, password, serverAddress, port);
+	local serverStatusMsg = String.Mid(serverStatus, 4, -1);
+
+	Label.SetVisible("LB_SERVER_STATUS", true);
+	Image.SetVisible("IMG_SERVER", true);
+
+	if String.Left(serverStatus , 3) == "[8]" then
+		Label.SetText("LB_SERVER_STATUS", "MySQL Server error @"..dbAddress);
+		Image.Load("IMG_SERVER_STATUS", "AutoPlay\\htdocs\\images\\icons\\server_ko.png");
+		Dialog.Message("SQL Error", "There is a problem with the SQL Server : @"..dbAddress.."\r\n"..serverStatusMsg, MB_OK, MB_ICONSTOP, MB_DEFBUTTON1);
+		Application.ExitScript();
+	end
+
+	if String.Left(serverStatus , 3) ~= "[4]" then
+		Label.SetText("LB_SERVER_STATUS", "MySQL Server "..serverStatusMsg.." @ "..dbAddress);
+		Image.Load("IMG_SERVER_STATUS", "AutoPlay\\htdocs\\images\\icons\\server_ko.png");
+		Application.ExitScript();
+	else
+		Label.SetText("LB_SERVER_STATUS", "MySQL Server "..serverStatusMsg.." @ "..dbAddress);
+		Image.Load("IMG_SERVER_STATUS", "AutoPlay\\htdocs\\images\\icons\\server_ok.png");
+	end
 end
 
 function setDbConfigForm (currentObject, mode)
@@ -96,11 +154,6 @@ function saveDatabaseConfiguration ()
 		Application.SetLastError(0616);
 	end
 
-	if serverAddress == "" or serverAddress == "Address" then
-		Image.Load("IMG_DB_ADDRESS_DBCF", "AutoPlay\\htdocs\\images\\inputs\\input_mandatory.png");
-		Application.SetLastError(0616);
-	end
-
 	if port == "" or port == "Port" then
 		Image.Load("IMG_DB_PORT_DBCF", "AutoPlay\\htdocs\\images\\inputs\\input_mandatory.png");
 		Application.SetLastError(0616);
@@ -109,13 +162,40 @@ function saveDatabaseConfiguration ()
 		if portIsValid == 0 then
 			Dialog.Message("Error", "Invalid port number.", MB_OK, MB_ICONSTOP, MB_DEFBUTTON1);
 			Image.Load("IMG_DB_PORT_DBCF", "AutoPlay\\htdocs\\images\\inputs\\input_mandatory.png");
-			Application.ExitScript();
+			Application.SetLastError(0616);
 		end
+	end
+
+	if serverAddress == "" or serverAddress == "Address" then
+		Image.Load("IMG_DB_ADDRESS_DBCF", "AutoPlay\\htdocs\\images\\inputs\\input_mandatory.png");
+		Application.SetLastError(0616);
 	end
 
 	error = Application.GetLastError();
 	if (error == 0616) then
 		Application.ExitScript();
+	end
+
+	local serverStatus = checkSQLServerStatus(databaseName, userName, password, serverAddress, port);
+	local serverStatusMsg = String.Mid(serverStatus, 4, -1);
+
+	Label.SetVisible("LB_SERVER_STATUS", true);
+	Image.SetVisible("IMG_SERVER", true);
+
+	if String.Left(serverStatus , 3) == "[8]" then
+		Label.SetText("LB_SERVER_STATUS", "MySQL Server error @"..serverAddress);
+		Image.Load("IMG_SERVER_STATUS", "AutoPlay\\htdocs\\images\\icons\\server_ko.png");
+		Dialog.Message("SQL Error", "There is a problem with the SQL Server : @"..serverAddress.."\r\n"..serverStatusMsg, MB_OK, MB_ICONSTOP, MB_DEFBUTTON1);
+		Application.ExitScript();
+	end
+
+	if String.Left(serverStatus , 3) ~= "[4]" then
+		Label.SetText("LB_SERVER_STATUS", "MySQL Server "..serverStatusMsg.." @ "..serverAddress);
+		Image.Load("IMG_SERVER_STATUS", "AutoPlay\\htdocs\\images\\icons\\server_ko.png");
+		Application.ExitScript();
+	else
+		Label.SetText("LB_SERVER_STATUS", "MySQL Server "..serverStatusMsg.." @ "..serverAddress);
+		Image.Load("IMG_SERVER_STATUS", "AutoPlay\\htdocs\\images\\icons\\server_ok.png");
 	end
 
 	Image.SetVisible("IMG_DB_NAME_DBCF", false);
